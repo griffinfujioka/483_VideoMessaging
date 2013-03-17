d09b836accff1fd2b5993b92573f56ac14bac4a6
Griffin Fujioka <fujiokag@hotmail.com>
2013-03-16 20:37:29 -0700

    initial commit
    I created the project. I used the pivot template because I think it's
    flexible and will fit well for what we're doing. I'm sure we can change
    that later if we decide to.

---
+++ 483_VideoMessaging/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using _483_VideoMessaging.Resources;
+
+namespace _483_VideoMessaging
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
